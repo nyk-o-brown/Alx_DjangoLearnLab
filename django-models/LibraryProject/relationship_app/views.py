29926304c1a9ff119f06319bca3b9ebb5af954ae
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-07 22:07:17 +0300

    msg: import detail view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Book
 from .models import Library
+from django.views.generic.detail import DetailView
 
 def list_books(request):
     books = Book.objects.all()
