2af70ac4a12260b98071e543f7f6f7ba23a5b1b9
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-07 23:44:36 +0300

    msg:import login

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -7,6 +7,8 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
 from django.urls import reverse_lazy
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 
 
