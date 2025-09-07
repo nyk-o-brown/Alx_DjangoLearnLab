06fbef14c950e07073028aa348f4853581fea944
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-07 22:12:21 +0300

    msg: urls also import models views

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from .views import list_books
+from django.views.generic.detail import DetailView
 
 urlpatterns = [
     path('books/', views.list_books, name='list_books'),
