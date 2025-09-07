fa4523dac0f79b2373ae4670e73ec06bb0bf2e63
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-07 21:57:44 +0300

    msg:got the def and class view

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Book, Library
 
-# Create your views here.
+def list_books(request):
+    books = Book.objects.select_related('author').all()
+    return render(request, 'relationship_app/book_list.html', {'books': books})
+
+
+class LibraryDetailView(DetailView):
+    model = Library
+    template_name = 'relationship_app/library_detail.html'
+    context_object_name = 'library'
