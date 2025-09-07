5b3d2afd4241d8990836c3b99fd331c42a469412
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-07 22:02:18 +0300

    msg:list_books.html

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,7 +3,7 @@ from .models import Book, Library
 
 def list_books(request):
     books = Book.objects.all()
-    return render(request, 'relationship_app/book_list.html', {'books': books})
+    return render(request, 'relationship_app/list_books.html', {'books': books})
 
 
 class LibraryDetailView(DetailView):
