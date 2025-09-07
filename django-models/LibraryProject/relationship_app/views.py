b47d38b333c1da950bb72e3411aa640de4e1e783
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-07 22:04:05 +0300

    msg:import models sepratly

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from .models import Book, Library
+from .models import Book
+from .models import Library
 
 def list_books(request):
     books = Book.objects.all()
