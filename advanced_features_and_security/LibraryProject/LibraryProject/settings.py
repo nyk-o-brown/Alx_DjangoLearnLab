0926dc6af85fbcb8c2da6f93390c396e55246644
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-14 17:41:02 +0300

    msg:hey

--- advanced_features_and_security/LibraryProject/LibraryProject/settings.py
+++ advanced_features_and_security/LibraryProject/LibraryProject/settings.py
@@ -30,6 +30,15 @@ SECURE_BROWSER_XSS_FILTER = True  # Enables browser's XSS protection
 X_FRAME_OPTIONS = 'DENY'  # Prevents clickjacking by blocking iframe embedding
 SECURE_CONTENT_TYPE_NOSNIFF = True  # Stops browsers from guessing content types
 SECURE_SSL_REDIRECT = True
+SECURE_HTSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
+SECURE_HSTS_PRELOAD= True
+SESSION_COOKIE_SECURE= True
+CSRF_COOKIE_SECURE =True
+
+
+
+
 
 # Secure cookies (only sent over HTTPS)
 CSRF_COOKIE_SECURE = True
