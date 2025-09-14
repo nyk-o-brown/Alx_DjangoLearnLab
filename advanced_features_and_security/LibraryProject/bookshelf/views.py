be0d73a18ebbb63f77e7def23ed20fdb5f90cd44
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-14 17:22:08 +0300

    msg:finished views.py

--- advanced_features_and_security/LibraryProject/bookshelf/views.py
+++ advanced_features_and_security/LibraryProject/bookshelf/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
 from .models import Book
+from django.contrib.auth.decorators import permission_required
+
+
+@permission_required('books.can_edit', raise_exception=True)
+def edit_book(request, book_id):
+    book = Book.objects.get(id=book_id)
+    # logic to edit the book
+    return render(request, 'edit_book.html', {'book': book})
 
 
 def book_list(request):
@@ -15,3 +23,6 @@ def book_detail(request, book_id):
         return render(request, 'bookshelf/book_detail.html', {'book': book})
     except Book.DoesNotExist:
         return render(request, 'bookshelf/book_not_found.html', status=404)
+
+
+def edit_book()        
