d058e7a9ef0be0c1aca14a3e67caeed1d0781c45
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-21 21:41:40 +0300

    msg:fk

--- api_project/settings.py
+++ api_project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'api_project.urls'
+ROOT_URLCONF = 'urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'api_project.wsgi.application'
+WSGI_APPLICATION = 'wsgi.application'
 
 
 # Database
