3bd05dcbb0fa4f538bb3e13d5706b43f74ddc508
nyk-o-brown <odoyobrown3@gmail.com>
2025-09-28 22:52:10 +0300

    msg:fixed the api route issue

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -1,12 +1,19 @@
 from django.urls import path
 from . import views
 
+app_name = 'api'
+
 urlpatterns = [
-    # Book endpoints
+    # API endpoints using DRF views
     path('books/', views.BookList.as_view(), name='book-list'),
     path('books/<int:pk>/', views.BookDetail.as_view(), name='book-detail'),
-    
-    # Author endpoints
     path('authors/', views.AuthorList.as_view(), name='author-list'),
     path('authors/<int:pk>/', views.AuthorDetail.as_view(), name='author-detail'),
+
+    # Traditional Django views for web interface
+    path('books/list/', views.BookListView.as_view(), name='book-list-view'),
+    path('books/<int:pk>/detail/', views.BookDetailView.as_view(), name='book-detail-view'),
+    path('books/create/', views.BookCreateView.as_view(), name='book-create'),
+    path('books/<int:pk>/update/', views.BookUpdateView.as_view(), name='book-update'),
+    path('books/<int:pk>/delete/', views.BookDeleteView.as_view(), name='book-delete'),
 ]
