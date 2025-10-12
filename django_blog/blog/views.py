6f87a733c60df590846e2e5db22f004c4b2fbb89
nyk-o-brown <odoyobrown3@gmail.com>
2025-10-12 03:52:24 +0300

    msg:auth setup done

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,34 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django import forms
+
+class CustomUserCreationForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ("username", "email", "password1", "password2")
+
+def register(request):
+    if request.method == "POST":
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect("login")
+    else:
+        form = CustomUserCreationForm()
+    return render(request, "blog/register.html", {"form": form})
+
+@login_required
+def profile(request):
+    if request.method == "POST":
+        request.user.email = request.POST.get("email")
+        request.user.save()
+    return render(request, "blog/profile.html", {"user": request.user})
+
