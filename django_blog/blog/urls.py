26b9a321573061c273f6889160cfe3c060550eef
nyk-o-brown <odoyobrown3@gmail.com>
2025-10-12 06:36:53 +0300

    msg:look mama ive conqured the world

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,14 +1,17 @@
-from django.contrib.auth import views as auth_views
-
-#configure irl patterns
 from django.urls import path
 from . import views
 from django.contrib.auth import views as auth_views
 
+app_name = 'blog'
+
 urlpatterns = [
-    path("", views.home, name="home"),
-    path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
-    path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
-    path("register/", views.register, name="register"),
-    path("profile/", views.profile, name="profile"),
+    path('', views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
+    path('register/', views.register, name='register'),
+    path('profile/', views.profile, name='profile'),
+    path('post/new/', views.PostCreateView.as_view(), name='post_new'),
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    path('post/<int:pk>/edit/', views.PostUpdateView.as_view(), name='post_edit'),
+    path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post_delete'),
 ]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,14 +1,18 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
 from django import forms
+from .models import Post
 
-#fixing url errs
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.urls import reverse_lazy
 
 def home(request):
-    return render(request, "blog/home.html")
+    posts = Post.objects.all().order_by('-created_date')
+    return render(request, "blog/home.html", {'posts': posts})
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
@@ -34,3 +38,38 @@ def profile(request):
         request.user.save()
     return render(request, "blog/profile.html", {"user": request.user})
 
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'category']
+    success_url = reverse_lazy('blog:home')
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    model = Post
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'category']
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    model = Post
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:home')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
