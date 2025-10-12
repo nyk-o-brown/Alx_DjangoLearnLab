bf0c030076675ce5d3a3662aa88a935fd755c81e
nyk-o-brown <odoyobrown3@gmail.com>
2025-10-12 05:09:40 +0300

    msg:i kinda give up

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,6 +6,7 @@ from . import views
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
+    path("", views.home, name="home"),
     path("login/", auth_views.LoginView.as_view(template_name="blog/login.html"), name="login"),
     path("logout/", auth_views.LogoutView.as_view(template_name="blog/logout.html"), name="logout"),
     path("register/", views.register, name="register"),
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
-
-# Create your views here.
-
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth.models import User
 from django import forms
+
+#fixing url errs
+
+def home(request):
+    return render(request, "blog/home.html")
 
 class CustomUserCreationForm(UserCreationForm):
     email = forms.EmailField(required=True)
